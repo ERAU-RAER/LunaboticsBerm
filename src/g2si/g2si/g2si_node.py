24f7ce27675032f594d7749bfcd9558c3356a655
jake161 <62108812+jake161@users.noreply.github.com>
2025-03-06 00:14:44 -0500

    Cleaned up formatting and added log messages

--- src/g2si/g2si/g2si_node.py
+++ src/g2si/g2si/g2si_node.py
@@ -7,30 +7,25 @@ class g2siNode(Node):
     def __init__(self):
         super().__init__('g2si_node')
 
-        # Publisher to publish the new IMU data with converted acceleration in m/s²
-        self.publisher_ = self.create_publisher(Imu, 'imu_with_acceleration_m_s2', 10)
-
-        # Subscriber to the IMU topic
-        self.subscription = self.create_subscription(
-            Imu,
-            '/imu/data',  # Replace with your IMU topic name
-            self.listener_callback,
-            10
-        )
+        # Setup
+        self.publisher_ = self.create_publisher(Imu, '/imu/data', 10) # Publisher to publish the new IMU data with converted acceleration in m/s²
+        self.subscription = self.create_subscription(Imu, '/imu/data_raw',self.listener_callback,10) # Subscriber to the IMU topic
         self.subscription  # prevent unused variable warning
 
+        self.data_received = False
+        self.timer = self.create_timer(5.0, self.check_data_received)
+
+        self.get_logger().info(f'Converting IMU linear acceleration from g\'s to m/s²')
+
     def listener_callback(self, msg):
+        if not self.data_received:
+            self.data_received = True
+            self.get_logger().info('Found you! Received first IMU sample')
+
         # Convert linear acceleration from g's to m/s^2
-        # 1 g = 9.81 m/s^2
         converted_acceleration = self.convert_g_to_ms2(msg.linear_acceleration)
 
-        # Log the conversion for debugging
-        self.get_logger().info(f'Converted IMU linear acceleration from g\'s to m/s²')
-
-        # Create a new IMU message to publish
         new_imu_msg = Imu()
-
-        # Copy the rest of the IMU message data
         new_imu_msg.header = msg.header
         new_imu_msg.orientation = msg.orientation
         new_imu_msg.angular_velocity = msg.angular_velocity
@@ -44,7 +39,7 @@ class g2siNode(Node):
     def convert_g_to_ms2(self, accel_in_gs: Vector3) -> Vector3:
         """
         Converts the acceleration from g's to m/s^2.
-        :param accel_in_gs: Acceleration in g's (x, y, z components)
+        :input accel_in_gs: Acceleration in g's (x, y, z components)
         :return: Converted acceleration in m/s^2
         """
         conversion_factor = 9.81  # 1 g = 9.81 m/s^2
@@ -53,6 +48,10 @@ class g2siNode(Node):
         accel_in_ms2.y = accel_in_gs.y * conversion_factor
         accel_in_ms2.z = accel_in_gs.z * conversion_factor
         return accel_in_ms2
+
+    def check_data_received(self):
+        if not self.data_received:
+            self.get_logger().warn('Hello? Is anyone out there? Still waiting on first IMU sample.')
 
 def main(args=None):
     rclpy.init(args=args)
