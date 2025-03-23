f78929ec973579f63b5f922a33463cdad7c32deb
jake161 <62108812+jake161@users.noreply.github.com>
2025-03-22 23:18:12 -0400

    update package

--- src/bird_bringup/launch/bird_bringup_launch.py
+++ src/bird_bringup/launch/bird_bringup_launch.py
@@ -40,8 +40,8 @@ def generate_launch_description():
     )
 
     joint_state_publisher_node = Node(
-        package='joint_state_publisher_gui',
-        executable='joint_state_publisher_gui',
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
         name='joint_state_publisher',
         output='screen'
     )
