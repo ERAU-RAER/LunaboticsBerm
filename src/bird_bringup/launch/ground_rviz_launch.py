d0f627119d10f270c288dc05d71f81029823a897
jake161 <62108812+jake161@users.noreply.github.com>
2025-03-22 23:41:39 -0400

    update rviz path

--- src/bird_bringup/launch/ground_rviz_launch.py
+++ src/bird_bringup/launch/ground_rviz_launch.py
@@ -4,8 +4,8 @@ from launch import LaunchDescription
 from launch_ros.actions import Node
 
 cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
-cur_config_path = cur_path + '../config'
-rviz_config_path = os.path.join(cur_config_path, 'display_point_cloud_ROS2.rviz')
+cur_rviz_path = cur_path + '../rviz'
+rviz_config_path = os.path.join(cur_rviz_path, 'display_point_cloud_ROS2.rviz')
 
 def generate_launch_description():
 
