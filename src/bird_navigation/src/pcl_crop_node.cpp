1c4ee3bb7785e6f4833b3b4c15c0a83b50c4d992
jake161 <62108812+jake161@users.noreply.github.com>
2025-04-19 15:20:24 -0400

    AHHH H POINT CLOUDS FRAMES AAHHH

--- src/bird_navigation/src/pcl_crop_node.cpp
+++ src/bird_navigation/src/pcl_crop_node.cpp
@@ -6,6 +6,10 @@
 #include <pcl/filters/crop_box.h>
 #include <pcl/filters/passthrough.h>
 #include <Eigen/Dense>
+#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.h>
+#include <geometry_msgs/msg/transform_stamped.hpp>
+#include <tf2/exceptions.h>
 
 // Bring specific PCL classes and functions into the global namespace
 using pcl::PointCloud;
@@ -32,7 +36,7 @@ using sensor_msgs::msg::PointCloud2;
 
 class CropNode : public Node {
 public:
-    CropNode() : Node("crop_node") {
+    CropNode() : Node("crop_node"), tf_buffer_(get_clock()), tf_listener_(tf_buffer_) {
         // Create a subscription for input point clouds
         point_cloud_subscription_ = create_subscription<PointCloud2>(
             "input_cloud", 10,
@@ -46,24 +50,40 @@ public:
 
 private:
     void processPointCloud(const PointCloud2::SharedPtr msg) {
+        double delta_z = 0.0;
+        try {
+            // Lookup transform with 4 arguments as required by ROS 2 Humble
+            auto transformStamped = tf_buffer_.lookupTransform(
+                "base_footprint", "livox_frame",
+                rclcpp::Time(0),                         // time = latest available
+                tf2::durationFromSec(0.1));              // timeout as tf2::Duration
+            
+            delta_z = transformStamped.transform.translation.z;
+            // RCLCPP_INFO(get_logger(), "Delta Z between frames: %f", delta_z);
+        } catch (const tf2::TransformException &ex) {
+            RCLCPP_WARN(get_logger(), "Could not transform livox_frame to base_footprint: %s", ex.what());
+        }
+        
         // Convert ROS PointCloud2 message to PCL PointCloud using PointXYZI
         PointCloud<PointXYZI>::Ptr cloud(new PointCloud<PointXYZI>());
         fromROSMsg(*msg, *cloud);
         
-        // Cut up the input cloud
+        // Cut up the input cloud using adjusted z ranges with delta_z offset
         PointCloud<PointXYZI>::Ptr cloud_filtered_upper(new PointCloud<PointXYZI>());
         PointCloud<PointXYZI>::Ptr cloud_filtered_lower(new PointCloud<PointXYZI>());
 
-        PassThrough<pcl::PointXYZI> pass_upper; // Upper Band
+        // Upper Band: subtract offset from original limits [0.05, 0.25]
+        PassThrough<pcl::PointXYZI> pass_upper; 
         pass_upper.setInputCloud(cloud);
         pass_upper.setFilterFieldName("z");
-        pass_upper.setFilterLimits(0.05, 0.25);
+        pass_upper.setFilterLimits(0.05 + delta_z, 0.25 + delta_z);
         pass_upper.filter(*cloud_filtered_upper);
 
-        PassThrough<pcl::PointXYZI> pass_lower; // Lower Band
+        // Lower Band: subtract offset from original limits [-0.25, -0.05]
+        PassThrough<pcl::PointXYZI> pass_lower; 
         pass_lower.setInputCloud(cloud);
         pass_lower.setFilterFieldName("z");
-        pass_lower.setFilterLimits(-0.25, -0.05);
+        pass_lower.setFilterLimits(-0.25 + delta_z, -0.05 + delta_z);
         pass_lower.filter(*cloud_filtered_lower);
 
         // Combine the filtered clouds
@@ -85,16 +105,17 @@ private:
     
         // Publish the cropped point cloud
         cropped_publisher_->publish(output_msg);
-        // RCLCPP_INFO(get_logger(), "*CROPPING!*");
     }
 
     Publisher<PointCloud2>::SharedPtr cropped_publisher_;
     Subscription<PointCloud2>::SharedPtr point_cloud_subscription_;
+    tf2_ros::Buffer tf_buffer_;
+    tf2_ros::TransformListener tf_listener_;
 };
 
 int main(int argc, char **argv) {
     init(argc, argv);
-    auto node = make_shared<CropNode>();
+    auto node = std::make_shared<CropNode>();
     spin(node);
     shutdown();
     return 0;
